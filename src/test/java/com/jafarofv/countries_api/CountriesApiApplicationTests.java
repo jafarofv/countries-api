1373dc2a38e0b2605258499d8f73d79967c1b72a
jafarofv <eko.baknec@gmail.com>
2025-03-22 01:05:10 +0300

    Project architecture created.

---
+++ src/test/java/com/jafarofv/countries_api/CountriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafarofv.countries_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
